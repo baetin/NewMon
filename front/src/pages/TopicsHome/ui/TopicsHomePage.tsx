0b53db775134c0156106f2fa3b411e51c5d33e4f
jihwan2001 <coghtoy@naver.com>
2025-11-12 01:43:25 +0900

    refactor: topicHome 페이지 재정비

--- front/src/pages/TopicsHome/ui/TopicsHomePage.tsx
+++ front/src/pages/TopicsHome/ui/TopicsHomePage.tsx
@@ -6,15 +6,50 @@ import {
   TopicsLeft,
   Right,
 } from "../../../shared/styles/articleContents.styles";
+import { useEffect, useState } from "react";
+import type { ArticleDataTypes } from "../../../shared/types/Article.types";
+import { getArticles } from "../api/getArticles";
+import { topicMap } from "../model/topics.constants";
+import { useParams } from "react-router-dom";
+import { Spinner } from "../../../shared/ui";
 
 const TopicsHomePage: React.FC = () => {
+  const [articles, setArticles] = useState<ArticleDataTypes[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const { topic } = useParams<{ topic: string }>();
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        if (!topic) return;
+        const topicId = topicMap[topic.toLowerCase()];
+        const data = await getArticles(topicId);
+        setArticles(data);
+        console.log("불러온 데이터 : ", data);
+      } catch (error) {
+        console.error("기사 불러오기 실패:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchArticles();
+  }, [topic]);
+
+  if (loading) return <Spinner />;
+  if (!articles.length) return <div>게시글이 없습니다.</div>;
+
+  const mainArticle = articles[0];
+  const subArticles = articles.slice(1);
+
   return (
     <Container>
       <TopicsLeft>
-        <TopicsMainArticle />
+        <TopicsMainArticle article={mainArticle} />
       </TopicsLeft>
       <Right>
-        <TopicsSubArticleList />
+        <TopicsSubArticleList articles={subArticles} />
       </Right>
     </Container>
   );
--- front/src/widgets/TopicsHome/ui/TopicsMainArticle/TopicsMainArticle.tsx
+++ front/src/widgets/TopicsHome/ui/TopicsMainArticle/TopicsMainArticle.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import {
   ArticleContainer,
@@ -8,39 +8,15 @@ import {
   Contents,
 } from "./TopicsMainArticle.styles";
 import ExpandedArticle from "../../../../features/article-expand/ui/ExpandedArticle";
-import { getArticles } from "../../api/getArticles";
 import type { ArticleDataTypes } from "../../../../shared/types/Article.types";
-import { useParams } from "react-router-dom";
 import { Spinner } from "../../../../shared/ui";
-import { topicMap } from "../../model/topics.constants";
 
-const TopicsMainArticle: React.FC = () => {
+interface ArticleDataProps {
+  article: ArticleDataTypes;
+}
+
+const TopicsMainArticle: React.FC<ArticleDataProps> = ({ article }) => {
   const [selected, setSelected] = useState<boolean>(false);
-  const [article, setArticle] = useState<ArticleDataTypes | null>(null);
-  const { topic } = useParams<{ topic: string }>();
-
-  useEffect(() => {
-    const fetchArticles = async () => {
-      if (!topic) return;
-
-      const topicId = topicMap[topic.toLowerCase()];
-      if (!topicId) {
-        console.error("유효하지 않은 topic:", topic);
-        return;
-      }
-
-      try {
-        const data = await getArticles(topicId);
-        if (data.length > 0) {
-          setArticle(data[0]); // 첫번째 기사만
-        }
-      } catch (error) {
-        console.error("Failed to fetch article:", error);
-      }
-    };
-
-    fetchArticles();
-  }, [topic]);
 
   if (!article) return <Spinner />;
 
