23564620b71742d323fa1224a44298f3740cbb0a
jihwan2001 <coghtoy@naver.com>
2025-11-18 18:28:18 +0900

    chore: 관심종목 선택페이지 정리

--- front/src/pages/InterestSelect/api/handleUserInterests.ts
+++ front/src/pages/InterestSelect/api/handleUserInterests.ts
@@ -1,11 +1,28 @@
 import axios from "axios";
 
-export const handleUserInterests = async (interests: number[]) => {
+interface UserInterestsProps {
+  interests: number[];
+  navigate: (path: string) => void;
+}
+
+export const handleUserInterests = async ({
+  interests,
+  navigate,
+}: UserInterestsProps) => {
+  const token = sessionStorage.getItem("accessToken");
+
   try {
-    const response = await axios.post("/api/user/interests", {
-      interests,
-    });
+    const response = await axios.post(
+      "/api/user/interests",
+      { interests },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`, // oauth 프레임워크에서 사용하는 토큰 인증 방식 / 유효한 토큰인지 확인
+        },
+      }
+    );
     alert("관심 종목 설정에 성공했습니다.");
+    navigate("/");
     return response.data;
   } catch (err) {
     console.error("❌ 관심 종목 post 실패:", err);
