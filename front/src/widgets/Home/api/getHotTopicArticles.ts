f626590178e26dcdc491cce539528f4a1353076d
jihwan2001 <coghtoy@naver.com>
2025-11-27 18:13:16 +0900

    fix: hottopic, interesttopic에 원문(full_text) 추가

--- front/src/widgets/Home/api/getHotTopicArticles.ts
+++ front/src/widgets/Home/api/getHotTopicArticles.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import type { TopicArticle } from "../model/HotTopicArticle.types";
+import type { TopicArticle } from "../model/TopicArticle.types";
 
 export const getHotTopicArticles = async (): Promise<TopicArticle[]> => {
   try {
--- front/src/widgets/Home/model/TopicArticle.types.ts
+++ front/src/widgets/Home/model/TopicArticle.types.ts
@@ -2,6 +2,7 @@ export interface TopicArticle {
   article_id: number;
   title: string;
   summary_text: string | null;
+  full_text: string | null;
   image_url: string | null;
   published_date: string;
   topic_table: string;
