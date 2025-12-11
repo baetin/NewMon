2b3d85297903f541ebe484c194ed3792c1fb6658
jihwan2001 <coghtoy@naver.com>
2025-12-11 15:51:44 +0900

    refactor: seemore index.ts에 추가

--- front/src/shared/ui/index.ts
+++ front/src/shared/ui/index.ts
@@ -3,3 +3,5 @@ export { Spinner } from "./Spinner/Spinner";
 export { Footer } from "./Footer/Footer";
 
 export { Pagination } from "./Pagination/Pagination";
+
+export { SeeMore } from "./SeeMore/SeeMore";
