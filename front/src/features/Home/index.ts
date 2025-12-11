890d5baffaaf1f83e45fc0f2abde5ba425e679ea
jihwan2001 <coghtoy@naver.com>
2025-12-11 14:00:16 +0900

    refactor: homne쪽 index.ts 추가 import 간결하게 정리

---
+++ front/src/features/Home/index.ts
@@ -0,0 +1,13 @@
+// UI Components
+export { MainHotTopicArticle } from "./ui/MainHotTopicArticle/MainHotTopicArticle";
+export { MainInterestsArticle } from "./ui/MainInterestsArticle/MainInterestsArticle";
+export { MainRightSection } from "./ui/MainRightSection/MainRightSection";
+
+// Hooks
+export * from "./hooks/useHotTopicQuery";
+export * from "./hooks/useInterestsQuery";
+export * from "./hooks/useWeatherQuery";
+
+// Models & State
+export * from "./model/examplesState";
+export * from "./model/WeatherDataProps.types";
