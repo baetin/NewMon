82fae30c6eb58e592bc134516012604b4c45f407
jihwan2001 <coghtoy@naver.com>
2025-12-11 15:37:26 +0900

    refactor: topicHome 파일 구조 리펙토링

---
+++ front/src/features/topicsHome/index.ts
@@ -0,0 +1,13 @@
+// Pages
+export { default as TopicsHomePage } from "./pages/TopicsHomePage";
+
+// UI Components
+export { default as TopicsMainArticle } from "./ui/TopicsMainArticle/TopicsMainArticle";
+export { default as TopicsSubArticleList } from "./ui/TopicsSubArticleList/TopicsSubArticleList";
+
+// Hooks
+export * from "./hooks/useArticlesQuery";
+
+// Models / Constants
+export * from "./model/topics.constants";
+export * from "./model/GetArticlesResponseProps";
