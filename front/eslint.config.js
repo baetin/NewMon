9626fbb7bf49e758863281c74e4b442a25415051
jihwan2001 <coghtoy@naver.com>
2025-12-15 23:19:02 +0900

    refacotr: 새로운 prettier 도입

--- front/eslint.config.js
+++ front/eslint.config.js
@@ -1,17 +1,20 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default defineConfig([
   globalIgnores(['dist']),
+
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
+      react.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
@@ -19,5 +22,21 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      /* React 17+ */
+      'react/react-in-jsx-scope': 'off',
+
+      /* unused-vars: 커밋 안정성 */
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn'],
+
+      /* console 정책 */
+      'no-console': 'warn',
+    },
   },
-])
+]);
--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,5 +1,6 @@
-import { createRoot } from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { App } from "./app/App";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { createRoot } from 'react-dom/client';
 
-createRoot(document.getElementById("root")!).render(<App />);
+import { App } from './app/App';
+
+createRoot(document.getElementById('root')!).render(<App />);
