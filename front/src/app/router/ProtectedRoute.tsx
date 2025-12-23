19ab1304da13eb1c118074efe9195c392c8af457
jihwan2001 <coghtoy@naver.com>
2025-12-23 11:17:23 +0900

    fix: isLoading -> isPending

--- front/src/app/router/ProtectedRoute.tsx
+++ front/src/app/router/ProtectedRoute.tsx
@@ -1,16 +1,18 @@
-import type { JSX } from "react";
-import { Navigate } from "react-router-dom";
-import { useAuth } from "@/features/auth";
-import { Spinner } from "@/shared/ui";
+import type { JSX } from 'react';
+
+import { Navigate } from 'react-router-dom';
+
+import { useAuth } from '@/features/auth';
+import { Spinner } from '@/shared/ui';
 
 export const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
-  const { isAuthenticated, isLoading, isError } = useAuth();
-  if (isLoading) return <Spinner />;
+  const { isAuthenticated, isPending, isError } = useAuth();
+  if (isPending) return <Spinner />;
 
   if (isError) return;
 
   if (!isAuthenticated) {
-    return <Navigate to={"/login"} replace />;
+    return <Navigate to={'/login'} replace />;
   }
   return children;
 };
--- front/src/features/auth/hooks/useAuth.ts
+++ front/src/features/auth/hooks/useAuth.ts
@@ -1,14 +1,14 @@
-import { useSessionCheckQuery } from "./useSessionCheckQuery";
+import { useSessionCheckQuery } from './useSessionCheckQuery';
 
 // 현재 클라이언트 상태 읽기
 export const useAuth = () => {
-  const { data, isLoading, isError } = useSessionCheckQuery();
+  const { data, isPending, isError } = useSessionCheckQuery();
 
   return {
     isAuthenticated: data?.isAuthenticated ?? false,
-    isLoading,
+    isPending,
     userId: data?.userId ?? null,
-    displayName: data?.displayName ?? "",
+    displayName: data?.displayName ?? '',
     isNewUser: data?.isNewUser,
     isError,
   };
